0d7b1d012229efe2b93f675dd086c7c91e24f0df
Kevin <kyrodriguez97@gmail.com>
2018-01-15 21:36:46 -0500

    Adding diamonds

--- src/game.js
+++ src/game.js
@@ -1,3 +1,4 @@
+var AMOUNT_DIAMONDS = 30;
 Game = {
   init: function() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -7,7 +8,8 @@ Game = {
   },
   preload: function() {
     game.load.image('background', 'assets/images/background.png');
-    game.load.spritesheet('horse', 'assets/images/horse.png', 84, 156, 2)
+    game.load.spritesheet('horse', 'assets/images/horse.png', 84, 156, 2);
+    game.load.spritesheet('diamonds', 'assets/images/diamonds.png', 81, 84, 4);
   },
   create: function() {
     game.add.sprite(0, 0, 'background');
@@ -15,7 +17,26 @@ Game = {
     this.horse.x = game.width / 2;
     this.horse.y = game.height / 2;
     this.horse.anchor.setTo(0.5);
+
     game.input.onDown.add(this.onTap, this);
+    this.diamonds = [];
+    for (var i = 0; i < AMOUNT_DIAMONDS; i++) {
+      var diamond = game.add.sprite(100, 100, 'diamonds');
+      diamond.frame = game.rnd.integerInRange(0, 3);
+      diamond.scale.setTo(0.30 + game.rnd.frac());
+      diamond.anchor.setTo(0.5);
+      diamond.x = game.rnd.integerInRange(50, 1050);
+      diamond.y = game.rnd.integerInRange(50, 600);
+      this.diamonds[i] = diamond;
+      var rectCurrentDiamond = this.getBoundsDiamond(diamond);
+      var rectHorse = this.getBoundsDiamond(this.horse);
+
+      while (this.isOverlapingOtherDiamond(i, rectCurrentDiamond) || this.isRectanglesOverlapping(rectHorse, rectCurrentDiamond)) {
+        diamond.x = game.rnd.integerInRange(50, 1050);
+        diamond.y = game.rnd.integerInRange(50, 600);
+        rectCurrentDiamond = this.getBoundsDiamond(diamond);
+      }
+    }
   },
   update: function() {
     if (this.flagfirstMouseDown) {
@@ -35,8 +56,29 @@ Game = {
       this.horse.y += distY * 0.03;
     }
   },
-  onTap:function() {
+  onTap: function() {
     this.flagfirstMouseDown = true;
+  },
+  getBoundsDiamond: function(currentDiamond) {
+    return new Phaser.Rectangle(currentDiamond.left, currentDiamond.top, currentDiamond.width, currentDiamond.height);
+  },
+  isRectanglesOverlapping: function(rect1, rect2) {
+    if (rect1.x > rect2.x + rect2.width || rect2.x > rect1.x + rect1.width) {
+      return false;
+    }
+    if (rect1.y > rect2.y + rect2.height || rect2.y > rect1.y + rect1.height) {
+      return false;
+    }
+    return true;
+  },
+  isOverlapingOtherDiamond: function(index, rect2) {
+    for (var i = 0; i < index; i++) {
+      var rect1 = this.getBoundsDiamond(this.diamonds[i]);
+      if (this.isRectanglesOverlapping(rect1, rect2)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 
